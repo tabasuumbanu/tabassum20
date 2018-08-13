75381f3789330fce905ba57496fe33fe7619b1f5
tabasuumbanu <41725742+tabasuumbanu@users.noreply.github.com>
2018-08-13 13:01:42 +0530

    Create tab.java

---
+++ tab.java
@@ -0,0 +1,15 @@
+	Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int K=sc.nextInt();
+		int sum=0;
+		int a[]=new int[N];
+		for(int i=0;i<a.lenth;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<k;i++);
+		{
+			sum=sum+a[i];
+		}
+		System.out.println(sum);
+		
