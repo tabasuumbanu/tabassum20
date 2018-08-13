4dc4635adab49edf0eb696881c33071fc2ae9e37
tabasuumbanu <41725742+tabasuumbanu@users.noreply.github.com>
2018-08-13 12:59:24 +0530

    Create integer.java

---
+++ integer.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int K=sc.nextInt();
+		int sum=0;
+		int a[]=new int[N];
+		for(int i=0;i<a.lenth;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<k;i++);
+		{
+			sum=sum+a[i];
+		}
+		System.out.println(sum);
+		}
+}
